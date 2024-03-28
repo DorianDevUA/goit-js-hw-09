4890644d75557a2238dfdb63198bea7af2c51854
DorianDevUA <dorian.devua@gmail.com>
2024-03-28 19:13:26 +0400

    Adds HomeWork #9. Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+let intervalId = null;
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', onClickStartBtn);
+refs.stopBtn.addEventListener('click', onClickStopBtn);
+
+function onClickStartBtn() {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+  intervalId = setInterval(changeBackgroundColor, 1000);
+}
+
+function onClickStopBtn() {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeBackgroundColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
